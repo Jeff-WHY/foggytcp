45637df589a3f504a24b09d24486e0b549e384f7
Jeff-WHY <jeff.wonghy66@gmail.com>
2025-09-22 18:44:42 +0800

    CP1_testing

--- foggytcp/src/client.cc
+++ foggytcp/src/client.cc
@@ -1,10 +1,10 @@
 /* Copyright (C) 2024 Hong Kong University of Science and Technology
 
-This repository is used for the Computer Networks (ELEC 3120) 
-course taught at Hong Kong University of Science and Technology. 
+This repository is used for the Computer Networks (ELEC 3120)
+course taught at Hong Kong University of Science and Technology.
 
-No part of the project may be copied and/or distributed without 
-the express permission of the course staff. Everyone is prohibited 
+No part of the project may be copied and/or distributed without
+the express permission of the course staff. Everyone is prohibited
 from releasing their forks in any public places. */
 
 #include <unistd.h>
@@ -17,6 +17,8 @@ using namespace std;
 
 #define BUF_SIZE 4096
 
+// this is the CP1_testing
+
 /**
  * This file implements a simple TCP client. Its purpose is to provide simple
  * test cases and demonstrate how the sockets will be used.
@@ -27,24 +29,27 @@ using namespace std;
  * ./client 10.0.1.1 3120 test.in
  */
 
-int main(int argc, const char* argv[]) {
-  if (argc != 4) {
+int main(int argc, const char *argv[])
+{
+  if (argc != 4)
+  {
     cerr << "Usage: " << argv[0] << " <server-ip> <server-port> <filename>\n";
     return -1;
   }
 
-  const char* server_ip = argv[1];
-  const char* server_port = argv[2];
-  const char* filename = argv[3];
+  const char *server_ip = argv[1];
+  const char *server_port = argv[2];
+  const char *filename = argv[3];
   struct timespec start_time;
 
   /* Create an initiator socket */
-  void* sock = foggy_socket(TCP_INITIATOR, server_port, server_ip);
+  void *sock = foggy_socket(TCP_INITIATOR, server_port, server_ip);
 
   /* Open the input file. If the file can't be opened, print an error message
    * and return -1 */
   ifstream ifs(filename);
-  if (!ifs) {
+  if (!ifs)
+  {
     cerr << "Error: Can't open \"" << filename << "\"\n";
     return -1;
   }
@@ -54,24 +59,27 @@ int main(int argc, const char* argv[]) {
 
   char buf[BUF_SIZE];
   bool first_packet = true;
-  
-  while (ifs) {
+
+  while (ifs)
+  {
     /* Read data from the file into the buffer. The amount of data read is
      * stored in bytes_read */
     ifs.read(buf, BUF_SIZE);
     int bytes_read = ifs.gcount();
 
-    if (first_packet && bytes_read > 0) {
+    if (first_packet && bytes_read > 0)
+    {
       timespec_get(&start_time, TIME_UTC);
-      
+
       /* Insert timestamp into first packet */
       char timestamped_buf[BUF_SIZE + sizeof(struct timespec)];
       memcpy(timestamped_buf, &start_time, sizeof(start_time));
       memcpy(timestamped_buf + sizeof(start_time), buf, bytes_read);
-      
+
       /* Write timestamped first packet */
       int bytes_written = foggy_write(sock, timestamped_buf, bytes_read + sizeof(start_time));
-      if (bytes_written < 0) {
+      if (bytes_written < 0)
+      {
         cerr << "Error: Write failed\n";
         return -1;
       }
@@ -79,9 +87,11 @@ int main(int argc, const char* argv[]) {
       continue;
     }
 
-    if (bytes_read > 0) {
+    if (bytes_read > 0)
+    {
       int bytes_written = foggy_write(sock, buf, bytes_read);
-      if (bytes_written < 0) {
+      if (bytes_written < 0)
+      {
         cerr << "Error: Write failed\n";
         return -1;
       }
